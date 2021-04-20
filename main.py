0e988447445e055399bbfd478826575127df18bd
unknown <pati3051@gmail.com>
2021-04-20 16:41:11 +0200

    homework 1.2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, Response, status, Request
+# -*- coding: utf-8 -*-
+from fastapi import FastAPI, Response, status, Request, HTTPException
 from pydantic import BaseModel
 from hashlib import sha512
 from fastapi.responses import JSONResponse
 from typing import Optional
 from datetime import datetime, time, timedelta, date
 from fastapi.encoders import jsonable_encoder
+import re
 app = FastAPI()
 app.counter = 0
 app.id_reg = 1
@@ -30,17 +32,36 @@ def hello_name_view(name: str):
     return HelloResp(msg=f"Hello {name}")
 
 
+@app.put("/method")
+def method():
+    return {"method": "PUT"}
+
+
 @app.get("/method")
 def method():
     return {"method": "GET"}
 
+@app.post("/method", status_code=status.HTTP_201_CREATED)
+def method():
+    return {"method": "POST"}
+
+
+@app.delete("/method")
+def method():
+    return {"method": "DELETE"}
+
+@app.options("/method")
+def method():
+    return {"method": "OPTIONS"}
+
 @app.get("/auth")
-def auth(password:str,password_hash:str, response:Response):
+def auth(password: Optional[str] = None ,password_hash: Optional[str]= None):
     password_sha512 = sha512(password.encode())
     if password_sha512.hexdigest() == password_hash:
-        response.status_code = status.HTTP_204_NO_CONTENT
+        raise HTTPException(status_code=204)
     else:
-        response.status_code = status.HTTP_401_UNAUTHORIZED
+        raise HTTPException(status_code=401)
+
 
 class Patient(BaseModel):
     id: Optional[int]
@@ -49,12 +70,25 @@ class Patient(BaseModel):
     register_date: Optional[date]
     vaccination_date: Optional[date]
 
+def check_string(data):
+    new_data = data
+    for c in data:
+        print(c.isdigit())
+        if c.isalpha() == False:
+            new_data = new_data.replace(c,'')
+    new_data = new_data.translate(new_data.maketrans({'ó': 'o', 'ż': 'z','ź': 'z','ń':'n','ą':'a','ę':'e','ł':'l','ć':'c','ś':'s','Ó': 'O', 'Ż': 'Z','Ź': 'Z','Ń':'N','Ą':'A','Ę':'E','Ł':'L','Ć':'C','Ś':'S'}))
+    return new_data
+
+            
 @app.post("/register")
 async def register(patient: Patient, response: Response):
     patient.id = app.id_reg
-    patient.register_date = date.today()
+    patient.name = check_string(patient.name)
+    patient.surname = check_string(patient.surname)
+    patient.register_date = date.today().strftime('%Y-%m-%d')
     plus_data = len(patient.name)+len(patient.surname)
-    patient.vaccination_date = date.today()+timedelta(days=plus_data)
+    vaccination = date.today()+timedelta(days=plus_data)
+    patient.vaccination_date = vaccination.strftime('%Y-%m-%d')
     app.patient_list.append(patient) 
     app.id_reg +=1
     response.status_code = status.HTTP_201_CREATED
--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from fastapi.testclient import TestClient
 from datetime import datetime, time, timedelta, date
 import pytest
@@ -12,16 +13,6 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world!"}
 
-def test_method():
-    response = client.get(f"/method")
-    assert response.status_code == 200
-    assert response.json() == {"method": "GET"}
-
-def test_auth():
-    response = client.get(f"/auth?password=haslo&password_hash=013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215")
-    assert response.status_code == 204
-    response = client.get(f"/auth?password=haslo&password_hash=f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e09fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091")
-    assert response.status_code == 401
 
 @pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
 def test_hello_name(name):
@@ -42,6 +33,20 @@ def test_counter():
     response = client.get(f"/counter")
     assert response.status_code == 200
     assert response.text == "3"
+
+def test_method():
+    response = client.get(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
+
+def test_auth():
+    response = client.get(f"/auth?password=haslo&password_hash=013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215")
+    assert response.status_code == 204
+    response = client.get(f"/auth?password=haslo&password_hash=f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e09fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091")
+    assert response.status_code == 401
+    response = client.get(f"/auth?password=""&password_hash=""")
+    assert response.status_code == 401
+
 
 def test_register():
     response = client.post(f"/register", json={"name": "Jan", "surname": "Nowak"})
