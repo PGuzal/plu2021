0d750512c19735fff03995d39c389a0aa45d7009
unknown <pati3051@gmail.com>
2021-05-17 15:54:45 +0200

    Homework 5.1.8

--- others/schemas.py
+++ others/schemas.py
@@ -18,17 +18,17 @@ class Supplier(BaseModel):
 
 class Supplier2(BaseModel):
     SupplierID: PositiveInt
-    CompanyName: Optional[constr(max_length=60)]
-    ContactName: Optional[constr(max_length=60)]
-    ContactTitle: Optional[constr(max_length=60)]
+    CompanyName: Optional[constr(max_length=40)]
+    ContactName: Optional[constr(max_length=30)]
+    ContactTitle: Optional[constr(max_length=30)]
     Address: Optional[constr(max_length=60)]
-    City: Optional[constr(max_length=60)]
-    Region: Optional[constr(max_length=60)]
-    PostalCode: Optional[constr(max_length=60)]
-    Country: Optional[constr(max_length=60)]
-    Phone: Optional[constr(max_length=60)]
-    Fax: Optional[constr(max_length=60)]
-    HomePage: Optional[constr(max_length=60)]
+    City: Optional[constr(max_length=15)]
+    Region: Optional[constr(max_length=15)]
+    PostalCode: Optional[constr(max_length=10)]
+    Country: Optional[constr(max_length=15)]
+    Phone: Optional[constr(max_length=24)]
+    Fax: Optional[constr(max_length=24)]
+    HomePage: Optional[str]
 
     class Config:
         orm_mode = True
