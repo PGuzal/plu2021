c9830a197f83dd6e06c4cac0b5be6c8e97806819
unknown <pati3051@gmail.com>
2021-05-17 18:39:42 +0200

    Homework 5.2.13

--- others/schemas.py
+++ others/schemas.py
@@ -45,7 +45,8 @@ class Prod_supl(BaseModel):
     ProductID: PositiveInt
     ProductName: Optional[constr(max_length=40)]
     ContactName: Optional[constr(max_length=30)]
-    Category: List[CategoryData] = {}
+    #Category: List[CategoryData] = {}
+    CategoryName:Optional[constr(max_length=30)]
     Discontinued: Optional[constr(max_length=30)]
 
     class Config:
