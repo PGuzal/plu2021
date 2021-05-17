cb1fcbafbfe5f9a16c0242bb602728c116a23080
unknown <pati3051@gmail.com>
2021-05-17 21:53:11 +0200

    help

---
+++ Nowy folder/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Depends, HTTPException
+
+from others.views import router as northwind_api_router
+
+# from typing import List
+
+from sqlalchemy.orm import Session
+
+from others import crud, schemas
+from others.database import get_db
+app = FastAPI()
+
+app.include_router(northwind_api_router, tags=["northwind"])
+
+
+
