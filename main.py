e38cf87e56c30ef8e1a17c5a0d94cfe880d03e8e
unknown <pati3051@gmail.com>
2021-05-17 14:58:29 +0200

    Homework 5.1.2

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException
 
-from .views import router as northwind_api_router
+from others.views import router as northwind_api_router
 
 from typing import Lis
 
 from sqlalchemy.orm import Session
 
-from . import crud, schemas
-from .database import get_db
+from others import crud, schemas
+from others.database import get_db
 app = FastAPI()
 
 app.include_router(northwind_api_router, tags=["northwind"])
